5c7b301c2d84c1683d0d7220b872e4ff14413541
newgene <anewgene@yahoo.com>
2012-07-23 17:44:16 -0700

    some code re-org and adding more doc strings.

--- mygene/__init__.py
+++ mygene/__init__.py
@@ -45,7 +45,7 @@ def list2dict(list,keyitem,alwayslist=False):
             current_value.append(value)
             dict[key]=current_value
     return dict
-    
+
 
 class MyGeneInfo():
     def __init__(self, url='http://mygene.info'):
@@ -55,7 +55,7 @@ class MyGeneInfo():
         self.h = httplib2.Http()
         self.max_query=1000
 
-        #check http://mygene.info/meta for the most updated species list
+        #check http://mygene.info/metadata for the most updated species list
         self.taxid_d = {'human': 9606,
                         'mouse': 10090,
                         'rat':   10116,
@@ -65,28 +65,28 @@ class MyGeneInfo():
                         'thale-cress':   3702,
                         'frog':   8364,
                         'pig': 9823,
-                       }  
+                       }
 
     def _get(self, url, params):
         debug = params.pop('debug', False)
         return_raw = params.pop('return_raw', False)
         _url = url + '?' + urllib.urlencode(params)
         res, con = self.h.request(_url)
-        if debug: 
+        if debug:
             return _url, res, con
         assert res.status == 200, (_url, res, con)
         if return_raw:
             return con
         else:
             return json.loads(con)
-        
+
 
     def _post(self, url, params):
         debug = params.pop('debug', False)
         return_raw = params.pop('return_raw', False)
         headers = {'content-type': 'application/x-www-form-urlencoded'}
         res, con = self.h.request(url, 'POST', body=urllib.urlencode(params), headers=headers)
-        if debug: 
+        if debug:
             return url, res, con
         assert res.status == 200, (url, res, con)
         if return_raw:
@@ -114,89 +114,13 @@ class MyGeneInfo():
             if verbose:
                 print "querying %d-%d..." % (i+1, min(i+step, len(query_li))),
             query_result = query_fn(query_li[i:i+step])
-            
+
             yield query_result
-            
+
             print
             if delay:
-                time.sleep(delay) 
+                time.sleep(delay)
 
-    def findgenes(self, id_li, scope, species='all', entrezonly=False, delay=1, step=1000, raw=False, returnall=False):
-        '''return matched entrez gene ids for input id_li,
-           parameter "scope" specifies the type of ids,
-            which can be either a string for a single type 
-            or a tuple of multiple type strings.
-           This is a wrapper for POST query of "/query" service.
-             @param raw:    if True, return a list of raw query results
-             @param returnall:  if True, return a dict of all related data.
-        '''
-        if type(scope) not in [types.ListType, types.TupleType]:
-            scope = [scope]
-        taxid = self.taxid_d.get(species, None)
-        out = []
-        res = {}
-        li_dup = []
-        li_missing = []
-        query_fn = lambda ids: self.querymany(qterms=ids, scope=scope)
-        for hits in self._repeated_query(query_fn, id_li, delay=delay, step=step):
-            if raw:
-                out.extend(hits['rows'])
-            else: 
-                rows = [(g['key'][0], g['id'], g.get('symbol',''), g.get('name','')) \
-                         for g in hits['rows'] \
-                         if (taxid is None or g['taxid'] == taxid) and \
-                            (entrezonly is False or self._is_entrez_id(g['id'])) ]
-                res.update(list2dict(rows, 0))
-        if not raw:
-            for id in id_li:
-                rows = res.get(id.lower(), None)
-                if rows:
-                    if type(rows) is types.ListType:
-                        out.append((id,) + tuple(['//'.join([str(row[i]) for row in rows]) for i in range(len(rows[0]))]) )
-                        li_dup.append(id) 
-                    else:
-                        out.append((id,)+rows)
-                else:
-                    out.append((id, ) + ('',)*3)
-                    li_missing.append(id)
-        print "Finished."
-        if len(li_dup)>0:
-            print "%d input ids found dup hits:" % len(li_dup)
-            print "\t"+str(li_dup)[:100]
-        if len(li_missing)>0:
-            print "%d input ids found no hit:" % len(li_missing)
-            print "\t"+str(li_missing)[:100]
-        if returnall:
-            return {'out': out, 'res':res, 'dup':li_dup, 'missing':li_missing}
-        else:
-            return out
-
-    def query(self, q, limit=None, skip=None, sort=None, **kwargs):
-        '''Return  the query result.
-           This is a wrapper for GET query of "/query?q=<query>" service.
-           Refer to http://mygene.info/doc/query_service for query syntax.
-        '''
-        kwargs.update({'q': q})
-        if limit: kwargs['limit'] = limit
-        if skip: kwargs['skip'] = skip
-        if sort: kwargs['sort'] = sort
-        _url = self.url + '/query'
-        return self._get(_url, kwargs)
-
-    def querymany(self, qterms, scope=None, **kwargs):
-        '''Return the batch query result.
-           This is a wrapper for POST query of "/query" service.
-           qterms is either a list of query terms or a string with comma-sepearated
-            query terms.
-           Refer to http://mygene.info/doc/query_service for query syntax.
-        '''
-        kwargs.update({'q': self._format_list(qterms)})
-        if scope:
-            kwargs['scope'] = self._format_list(scope)
-        _url = self.url + '/query'
-        #return kwargs
-        return self._post(_url, kwargs)
-        
     def getgene(self, geneid, filter='symbol,name', **kwargs):
         '''Return the gene object for the give geneid.
            This is a wrapper for GET query of "/gene/<geneid>" service.
@@ -221,3 +145,107 @@ class MyGeneInfo():
         if filter: kwargs['filter'] = self._format_list(filter)
         _url = self.url + '/gene'
         return self._post(_url, kwargs)
+
+    def query(self, q, limit=None, skip=None, sort=None, **kwargs):
+        '''Return  the query result.
+           This is a wrapper for GET query of "/query?q=<query>" service.
+           Refer to http://mygene.info/doc/query_service for query syntax.
+            @param limit:   the maximum number of results to return (with a cap
+                              of 1000 at the moment)
+            @param skip:    the number of results to skip
+            @param sort:    the comma-separated fields to sort on. Prefix with / for
+                             ascending order and \ for descending order (default as
+                             ascending)
+        '''
+        kwargs.update({'q': q})
+        if limit: kwargs['limit'] = limit
+        if skip: kwargs['skip'] = skip
+        if sort: kwargs['sort'] = sort
+        _url = self.url + '/query'
+        return self._get(_url, kwargs)
+
+    def querymany(self, qterms, scope=None, **kwargs):
+        '''Return the batch query result.
+           This is a wrapper for POST query of "/query" service.
+           Refer to http://mygene.info/doc/query_service for query syntax.
+
+            @param qterms: either a list of query terms or a string with comma-separated
+                            query terms.
+            @param scope:  type of types of identifiers, either a list or a comma-separated fields to specify type of
+                           input qterms, e.g. "entrezgene", "entrezgene,symbol", ["ensemblgene", "symbol"]
+                           refer to "http://mygene.info/doc/query_service#available_fields" for full list
+                           of fields.
+
+
+        '''
+        kwargs.update({'q': self._format_list(qterms)})
+        if scope:
+            kwargs['scope'] = self._format_list(scope)
+        _url = self.url + '/query'
+        #return kwargs
+        return self._post(_url, kwargs)
+
+    def findgenes(self, id_li, scope, species='all', entrezonly=False, delay=1,
+                   step=1000, raw=False, returnall=False):
+        '''return matched entrez gene ids for input id_li,
+           parameter "scope" specifies the type of ids,
+            which can be either a string for a single type
+            or a tuple of multiple type strings.
+           This is a wrapper for POST query of "/query" service.
+            @param id_li:       a list of identifers
+            @param scope:       type of types of identifiers, either a list or a comma-separated fields
+                                to specify type of input qterms, e.g. "entrezgene", "entrezgene,symbol",
+                                ["ensemblgene", "symbol"]
+                                refer to "http://mygene.info/doc/query_service#available_fields" for full
+                                list of fields.
+            @param species:     "all" or one of "human", "mouse", "rat", etc.
+                                 refer to http://mygene.info/metadata for all supported species.
+            @param entrezonly:  if True, return only matching entrez gene, otherwise, including matching
+                                 Ensemble-only genes (those have no matching entrez genes).
+            @param raw:         if True, return a list of raw query results
+            @param returnall:   if True, return a dict of all related data.
+        '''
+        if type(scope) not in [types.ListType, types.TupleType]:
+            scope = [scope]
+        taxid = self.taxid_d.get(species, None)
+        out = []
+        res = {}
+        li_dup = []
+        li_missing = []
+        query_fn = lambda ids: self.querymany(qterms=ids, scope=scope)
+        for hits in self._repeated_query(query_fn, id_li, delay=delay, step=step):
+            if raw:
+                out.extend(hits['rows'])
+            else:
+                rows = [(g['key'][0], g['id'], g.get('symbol',''), g.get('name','')) \
+                         for g in hits['rows'] \
+                         if (taxid is None or g['taxid'] == taxid) and \
+                            (entrezonly is False or self._is_entrez_id(g['id'])) ]
+                res.update(list2dict(rows, 0))
+        if not raw:
+            for id in id_li:
+                rows = res.get(id.lower(), None)
+                if rows:
+                    if type(rows) is types.ListType:
+                        out.append((id,) + tuple(['//'.join([str(row[i]) for row in rows]) for i in range(len(rows[0]))]) )
+                        li_dup.append(id)
+                    else:
+                        out.append((id,)+rows)
+                else:
+                    out.append((id, ) + ('',)*3)
+                    li_missing.append(id)
+        print "Finished."
+        if len(li_dup)>0:
+            print "%d input ids found dup hits:" % len(li_dup)
+            print "\t"+str(li_dup)[:100]
+        if len(li_missing)>0:
+            print "%d input ids found no hit:" % len(li_missing)
+            print "\t"+str(li_missing)[:100]
+        if returnall:
+            return {'out': out, 'res':res, 'dup':li_dup, 'missing':li_missing}
+        else:
+            return out
+
+
+
+
