f41fc81fe867b56ffa157cb0a605f0b2f729171b
newgene <anewgene@yahoo.com>
2013-07-09 16:26:29 -0700

    converted to use MyGene.info v2 API

--- mygene/__init__.py
+++ mygene/__init__.py
@@ -56,25 +56,24 @@ def safe_str(s, encoding='utf-8'):
     return _s
 
 
+def list_itemcnt(list):
+    '''Return number of occurrence for each type of item in the list.'''
+    x={}
+    for item in list:
+        if x.has_key(item):
+            x[item]+=1
+        else:
+            x[item]=1
+    return [(i,x[i]) for i in x]
+
+
 class MyGeneInfo():
-    def __init__(self, url='http://mygene.info/v1'):
+    def __init__(self, url='http://mygene.info/v2'):
         self.url = url
         if self.url[-1] == '/':
             self.url = self.url[:-1]
         self.h = httplib2.Http()
         self.max_query=1000
-
-        #check http://mygene.info/metadata for the most updated species list
-        self.taxid_d = {'human': 9606,
-                        'mouse': 10090,
-                        'rat':   10116,
-                        'fruitfly': 7227,
-                        'nematode':   6239,
-                        'zebrafish':   7955,
-                        'thale-cress':   3702,
-                        'frog':   8364,
-                        'pig': 9823,
-                       }
 
     def _get(self, url, params={}):
         debug = params.pop('debug', False)
@@ -115,23 +114,23 @@ class MyGeneInfo():
 
     def _format_list(self, a_list, sep=','):
         if type(a_list) in (types.ListType, types.TupleType):
-            _out = sep.join([str(x) for x in a_list])
+            _out = sep.join([safe_str(x) for x in a_list])
         else:
             _out = a_list     # a_list is already a comma separated string
         return _out
 
-    def _repeated_query(self, query_fn, query_li, delay=1, step=1000, verbose=True):
+    def _repeated_query(self, query_fn, query_li, delay=1, step=1000, verbose=True, **fn_kwargs):
         step = min(step, self.max_query)
         for i in range(0, len(query_li), step):
             is_last_loop = i+step >= len(query_li)
             if verbose:
                 print "querying %d-%d..." % (i+1, min(i+step, len(query_li))),
-            query_result = query_fn(query_li[i:i+step])
+            query_result = query_fn(query_li[i:i+step], **fn_kwargs)
 
             yield query_result
 
             if verbose:
-                print
+                print "done."
             if not is_last_loop and delay:
                 time.sleep(delay)
 
@@ -140,142 +139,147 @@ class MyGeneInfo():
         _url = self.url+'/metadata'
         return self._get(_url)
 
-    def getgene(self, geneid, filter='symbol,name', **kwargs):
+    def getgene(self, geneid, fields='symbol,name,taxid,entrezgene', **kwargs):
         '''Return the gene object for the give geneid.
            This is a wrapper for GET query of "/gene/<geneid>" service.
              @param geneid: entrez/ensembl gene id
-             @param filter: fields to return, a list of a comma-sep string
-                            if filter==None, full gene object is returned.
-           Refer to http://mygene.info/doc/anno_service for available filters.
+             @param fields: fields to return, a list of a comma-sep string
+                            if fields=="all", all available fields are returned.
+             @param species: optionally, you can pass comma-separated species names
+                              or taxonomy ids
+             @param filter: alias for fields
+
+           Ref: http://mygene.info/doc/annotation_service.html
         '''
-        if filter: kwargs['filter'] = self._format_list(filter)
+        if fields: kwargs['fields'] = self._format_list(fields)
+        if 'filter' in kwargs:
+            kwargs['fields'] = self._format_list(kwargs['filter'])
         _url = self.url + '/gene/' + str(geneid)
         return self._get(_url, kwargs)
 
-    def getgenes(self, geneids, filter='symbol,name', **kwargs):
+    def getgenes(self, geneids, fields='symbol,name,taxid,entrezgene', **kwargs):
         '''Return the list of gene object for the given list of geneids.
            This is a wrapper for POST query of "/gene" service.
              @param geneids: a list or comm-sep entrez/ensembl gene ids
-             @param filter: fields to return, a list of a comma-sep string
-                            if filter==None, full gene object is returned.
-          Refer to http://mygene.info/doc/anno_service for available filters.
+             @param fields: fields to return, a list of a comma-sep string
+                            if fields=="all", all available fields are returned.
+             @param species: optionally, you can pass comma-separated species names
+                              or taxonomy ids
+             @param filter: alias for fields
+          Ref: http://mygene.info/doc/annotation_service.html
         '''
         kwargs.update({'ids': self._format_list(geneids)})
-        if filter: kwargs['filter'] = self._format_list(filter)
+        if fields: kwargs['fields'] = self._format_list(fields)
+        if 'filter' in kwargs:
+            kwargs['fields'] = self._format_list(kwargs['filter'])
         _url = self.url + '/gene'
         return self._post(_url, kwargs)
 
-    def query(self, q, limit=None, skip=None, sort=None, **kwargs):
+    def query(self, q, **kwargs):
         '''Return  the query result.
            This is a wrapper for GET query of "/query?q=<query>" service.
-           Refer to http://mygene.info/doc/query_service for query syntax.
-            @param limit:   the maximum number of results to return (with a cap
-                              of 1000 at the moment)
-            @param skip:    the number of results to skip
-            @param sort:    the comma-separated fields to sort on. Prefix with / for
-                             ascending order and \ for descending order (default as
-                             ascending)
+            @param fields: fields to return, a list of a comma-sep string
+                            if fields=="all", all available fields are returned.
+            @param species: optionally, you can pass comma-separated species names
+                              or taxonomy ids. Default: human,mouse,rat.
+            @param size:   the maximum number of results to return (with a cap
+                              of 1000 at the moment). Default: 10.
+            @param skip:    the number of results to skip. Default: 0.
+            @param sort:    Prefix with "-" for descending order, otherwise in ascending order.
+                            Default: sort by matching scores in decending order.
+
+            Ref: http://mygene.info/doc/query_service.html
         '''
         kwargs.update({'q': q})
-        if limit: kwargs['limit'] = limit
-        if skip: kwargs['skip'] = skip
-        if sort: kwargs['sort'] = sort
         _url = self.url + '/query'
         return self._get(_url, kwargs)
 
-    def querymany(self, qterms, scope=None, **kwargs):
+    def _querymany_inner(self, qterms, **kwargs):
+        _kwargs = {'q': self._format_list(qterms)}
+        _kwargs.update(kwargs)
+        _url = self.url + '/query'
+        return self._post(_url, _kwargs)
+
+    def querymany(self, qterms, scopes=None, **kwargs):
         '''Return the batch query result.
            This is a wrapper for POST query of "/query" service.
-           Refer to http://mygene.info/doc/query_service for query syntax.
 
-            @param qterms: either a list of query terms or a string with comma-separated
-                            query terms.
-            @param scope:  type of types of identifiers, either a list or a comma-separated fields to specify type of
+            @param qterms: a list of query terms, or a string of comma-separated query terms.
+            @param scopes:  type of types of identifiers, either a list or a comma-separated fields to specify type of
                            input qterms, e.g. "entrezgene", "entrezgene,symbol", ["ensemblgene", "symbol"]
-                           refer to "http://mygene.info/doc/query_service#available_fields" for full list
+                           refer to "http://mygene.info/doc/query_service.html#available_fields" for full list
                            of fields.
-
-
-        '''
-        kwargs.update({'q': self._format_list(qterms)})
-        if scope:
-            kwargs['scope'] = self._format_list(scope)
-        _url = self.url + '/query'
-        #return kwargs
-        return self._post(_url, kwargs)
-
-    def findgenes(self, id_li, scope, species='all', entrezonly=False, delay=1,
-                   step=1000, raw=False, returnall=False):
-        '''return matched entrez gene ids for input id_li,
-           parameter "scope" specifies the type of ids,
-            which can be either a string for a single type
-            or a tuple of multiple type strings.
-           This is a wrapper for POST query of "/query" service.
-            @param id_li:       a list of identifers
-            @param scope:       type of types of identifiers, either a list or a comma-separated fields
-                                to specify type of input qterms, e.g. "entrezgene", "entrezgene,symbol",
-                                ["ensemblgene", "symbol"]
-                                refer to "http://mygene.info/doc/query_service#available_fields" for full
-                                list of fields.
-            @param species:     "all" or one of "human", "mouse", "rat", etc.
-                                 if input is an integer, it's interpreted as a taxonomy id (taxid).
-                                 refer to http://mygene.info/metadata for all supported species and taxids.
-                                 if input species names or taxids are not supported, it will be ignored and
-                                 use default "all".
-
+            @param fields: fields to return, a list of a comma-sep string
+                            if fields=="all", all available fields are returned.
+            @param species: optionally, you can pass comma-separated species names
+                              or taxonomy ids. Default: human,mouse,rat.
             @param entrezonly:  if True, return only matching entrez gene, otherwise, including matching
                                  Ensemble-only genes (those have no matching entrez genes).
-            @param raw:         if True, return a list of raw query results
-            @param returnall:   if True, return a dict of all related data.
-        '''
-        if type(scope) not in [types.ListType, types.TupleType]:
-            scope = [scope]
 
-        if type(species) is types.IntType:
-            #so it is a taxid
-            taxid = species if species in self.taxid_d.values() else None
-        else:
-            #otherwise, treat is as a species name
-            taxid = self.taxid_d.get(species, None)
+            @delay
+            @step
+            #@param raw:         if True, return a list of raw query results
+            @param returnall:   if True, return a dict of all related data, including dup. and missing qterms
+            #@asiter:            if True, return a iterator instead of a list
+            @verbose            if True (default), print out infomation about dup and missing qterms
+
+
+            Ref: http://mygene.info/doc/query_service.html
+
+        '''
+        if type(qterms) in types.StringTypes:
+            qterms = qterms.split(',')
+        if (not (type(qterms) in (types.ListType, types.TupleType) and len(qterms) > 0)):
+            raise ValueError('input "qterms" must be non-empty list or tuple.')
+
+        if scopes:
+            kwargs['scopes'] = self._format_list(scopes)
+        if 'scope' in kwargs:
+            #allow scope for back-compatibility
+            kwargs['scopes'] = self._format_list(kwargs['scope'])
+        if 'species' in kwargs:
+            kwargs['species'] = self._format_list(kwargs['species'])
+        raw = kwargs.pop('raw', False)
+        returnall = kwargs.pop('returnall', False)
+        delay = kwargs.pop('delay', 1)
+        step = kwargs.pop('step', 1000)
+        verbose = kwargs.pop('verbose', True)
 
         out = []
-        res = {}
-        li_dup = []
         li_missing = []
-        query_fn = lambda ids: self.querymany(qterms=ids, scope=scope)
-        for hits in self._repeated_query(query_fn, id_li, delay=delay, step=step):
-            if raw:
-                out.extend(hits['rows'])
-            else:
-                rows = [(g['key'][0], g['id'], g.get('symbol',''), g.get('name','')) \
-                         for g in hits['rows'] \
-                         if (taxid is None or g['taxid'] == taxid) and \
-                            (entrezonly is False or self._is_entrez_id(g['id'])) ]
-                res.update(list2dict(rows, 0))
-        if not raw:
-            for id in id_li:
-                rows = res.get(safe_str(id).lower(), None)
-                if rows:
-                    if type(rows) is types.ListType:
-                        out.append((id,) + tuple(['//'.join([str(row[i]) for row in rows]) for i in range(len(rows[0]))]) )
-                        li_dup.append(id)
-                    else:
-                        out.append((id,)+rows)
+        li_dup = []
+        li_query = []
+        query_fn = lambda qterms: self._querymany_inner(qterms, **kwargs)
+        for hits in self._repeated_query(query_fn, qterms, delay=delay, step=step, verbose=verbose):
+            out.extend(hits)
+            for hit in hits:
+                if hit.get('notfound', False):
+                    li_missing.append(hit['query'])
                 else:
-                    out.append((id, ) + ('',)*3)
-                    li_missing.append(id)
-        print "Finished."
-        if len(li_dup)>0:
-            print "%d input ids found dup hits:" % len(li_dup)
-            print "\t"+str(li_dup)[:100]
-        if len(li_missing)>0:
-            print "%d input ids found no hit:" % len(li_missing)
-            print "\t"+str(li_missing)[:100]
+                    li_query.append(hit['query'])
+        if verbose:
+            print "Finished."
+        #check dup hits
+        if li_query:
+            li_dup = [(query, cnt) for query, cnt in list_itemcnt(li_query) if cnt > 1]
+        del li_query
+
+        if verbose:
+            if li_dup:
+                print "%d input query terms found dup hits:" % len(li_dup)
+                print "\t"+str(li_dup)[:100]
+            if li_missing:
+                print "%d input query terms found no hit:" % len(li_missing)
+                print "\t"+str(li_missing)[:100]
         if returnall:
-            return {'out': out, 'res':res, 'dup':li_dup, 'missing':li_missing}
+            return {'out': out, 'dup':li_dup, 'missing':li_missing}
         else:
+            if verbose and (li_dup or li_missing):
+                print 'Pass "returnall=True" to return complete lists of duplicate or missing query terms.'
             return out
 
 
-
+    def findgenes(self, id_li, **kwargs):
+        ''' Deprecated! It's kept here as an alias of "querymany" method.'''
+        return self.querymany(id_li, **kwargs)
 
