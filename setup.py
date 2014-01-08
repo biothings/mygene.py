b3618fbfcfb12539a06d305cb9a90c54a7ba8605
newgene <anewgene@yahoo.com>
2014-01-08 11:42:20 -0800

    updated setup.py for v2.1.0 release.

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 import os
 from setuptools import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "mygene",
-    version = "2.0.1",
-    author = "Chunlei Wu",
-    author_email = "cwu@scripps.edu",
-    description = "Python Client for MyGene.Info services.",
-    license = "BSD",
-    keywords = "biology gene annotation web service client api",
-    url = "https://bitbucket.org/newgene/mygene",
+    name="mygene",
+    version="2.1.0",
+    author="Chunlei Wu",
+    author_email="cwu@scripps.edu",
+    description="Python Client for MyGene.Info services.",
+    license="BSD",
+    keywords="biology gene annotation web service client api",
+    url="https://bitbucket.org/newgene/mygene",
     packages=['mygene'],
     long_description=read('README.rst'),
     classifiers=[
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
         "Operating System :: POSIX",
