64a9bb6aed6d57c3da779cff9bd919c0d390413d
Jerry <xzhou@scripps.edu>
2019-06-30 22:03:34 -0700

    Update user-agent pattern

--- mygene/__init__.py
+++ mygene/__init__.py
@@ -1,9 +1,14 @@
 '''
 Python Client for MyGene.Info services
 '''
-from biothings_client import get_client, alwayslist
+from biothings_client import alwayslist, get_client
 
 __version__ = '3.1.0'
 
+
 class MyGeneInfo(get_client('gene', instance=False)):
-    pass
+
+    def __init__(self):
+        super(MyGeneInfo, self).__init__()
+        self.default_user_agent += ' mygene/' + __version__
+
